5bbddfbdeee5ae394a4844764758cb0c6867b6e2
Smajo Husic <smajo@sempro.no>
2016-11-06 18:23:29 +0100

    init

---
+++ storage/framework/views/4302377de27fbb36486de3e824fbaeb1b0c321f8.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="no">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    </head>
+    <body>
+
+        <div id="app">
+            <?php $__env->startSection('content'); ?>
+        </div>
+
+    </body>
+</html>
